fbff2316bb0621074d01c22b18f3469db799c234
rodgersk410 <rodgersk410@gmail.com>
2016-05-03 18:54:36 -0400

    same as last commit

--- src/main/java/anovaApp/AnovaDatabase.java
+++ src/main/java/anovaApp/AnovaDatabase.java
@@ -1,17 +1,14 @@
 package anovaApp;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class Database {
+public class AnovaDatabase {
 	
 	int integerJobId;
 	
@@ -36,83 +33,61 @@ public class Database {
     }
 	return session;
 	}
-	
-	/*
-	public Integer getIntegerJobId(){
-		Connection conn = this.getSqlLiteConnection();
-		
-		//TO DO: Check if there are records in the db
-		String sqlQueryTableCount = "Select COUNT(*) from AnovaResultsTable";
-		PreparedStatement TableCountQuery = conn.prepareStatement(sqlQueryTableCount);
-		int TableCountResult = TableCountQuery.executeQuery(sqlQueryTableCount).getInt(0);
-		
-		if(TableCountResult == 0){
-			
-			integerJobId = 1;
-		}
-		else{
-			integerJobId = TableCountResult + 1;
-		}
-		return integerJobId;
-	}
-	*/
-	
-	// TO DO: insert the Integer Id in the db before the calculation
-	// public insertIntegerId(){
-	
-	//insert IntegerId
-	//set status to 'Pending'
-//}
 
-	public void insertIndexValues(String featuresIndexes, 
+	//post the anova calculation result to the db based on integer job id
+	public void updateIndexValues(String featuresIndexes, 
 			String result2DArray, String significances) throws SQLException{
 		
 		Connection conn = this.getSqlLiteConnection();
 		integerJobId = getIntegerJobId();
 		
-		//TO DO: Query the db for the integer id
+		//Insert the Anova calculated result into the db
+		//TO DO: set status to Complete
 		String sql = "UPDATE AnovaResultsTable SET "
 				+ "featuresIndexes='" + featuresIndexes + "', result2DArray='" + result2DArray + "',"
 						+ "significances='" + significances + "'"
 								+ "WHERE integerJobId='" + integerJobId + "';";
-		//TO DO: set status to Complete
 		final Logger log = LoggerFactory.getLogger(this.getClass());
 		log.info(sql);
 
+		//Prepare statement and execute
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.executeUpdate();
 		conn.close();
 	}
 	
+	//when user submits the form, create a new record in the db
 	public void createBlankDbRow() throws SQLException{
 		
 		Connection conn = this.getSqlLiteConnection();
 		
-		//TO DO: Query the db for the integer id
+		//Creating a blank row to get the integer job id
+		//TO DO: set status to Pending
 		String sql = "INSERT INTO [AnovaResultsTable] DEFAULT VALUES;";
-		//TO DO: set status to Complete
 		final Logger log = LoggerFactory.getLogger(this.getClass());
 		log.info(sql);
 
+		//Prepare statement and execute
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.executeUpdate();
 		conn.close();
 	}
 	
+	//get integer job id so we can use for updating the table
 	public int getIntegerJobId() throws SQLException{
 		
 		Connection conn = this.getSqlLiteConnection();
 		
 		//TO DO: Query the db for the integer id
 		String sql = "SELECT COUNT(*) FROM AnovaResultsTable;";
-		//TO DO: set status to Complete
 		final Logger log = LoggerFactory.getLogger(this.getClass());
 		log.info(sql);
 
+		//Prepare statement, execute, and get the integer job id
 		PreparedStatement ps = conn.prepareStatement(sql);
 		integerJobId = ps.executeQuery().getInt(1);
 		conn.close();
-		log.info("Integer Job Id" + integerJobId);
+		log.info("Integer Job Id " + integerJobId);
 		
 		return integerJobId;
 	}
